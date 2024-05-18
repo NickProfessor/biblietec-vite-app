7ce55a6e56b5f2bccd52e436a94939d4d59cee6b
Nickollas <nickollassilva90@gmail.com>
2024-05-18 09:27:53 -0300

    feat: add Marco component

---
+++ src/components/Marco/index.jsx
@@ -0,0 +1,10 @@
+import "./_Marco.scss";
+
+export default function Marco({ img }) {
+  return (
+    <div className="marco">
+      <img src={img} alt="" />
+      <h4>Presente em mais de 1000 instituições de ensino</h4>
+    </div>
+  );
+}
