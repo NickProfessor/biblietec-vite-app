82dddaa95f79ce2eec2ac5d36f8aa355cd55541b
Nickollas <nickollassilva90@gmail.com>
2024-05-18 09:26:13 -0300

    feat: add list of images to make it easier

---
+++ src/assets/images/index.jsx
@@ -0,0 +1,17 @@
+import coracao from "./icone-coracao.svg";
+import foguete from "./icone-foguete.svg";
+import instituicao from "./icone-instituicao.svg";
+import livro from "./icone-livro.svg";
+import olho from "./icone-olho.svg";
+import selo from "./icone-selo.svg";
+import titulo from "./icone-titulo.svg";
+
+export default {
+  coracao,
+  foguete,
+  instituicao,
+  livro,
+  olho,
+  selo,
+  titulo,
+};
