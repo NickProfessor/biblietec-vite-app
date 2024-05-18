40c4115b8954f1fb0d9a19a59b98d39dbea9da5b
Nickollas <nickollassilva90@gmail.com>
2024-05-18 09:27:20 -0300

    refactor: add components in Sobre page

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,3 +1,44 @@
+import images from "../../assets/images";
+import "./_Sobre.scss";
+import Topico from "../../components/Topico";
+import Marco from "../../components/Marco";
 export default function Sobre() {
-  return <h1>Sobre</h1>;
+  return (
+    <div className="sobre">
+      <section>
+        <div className="rotulo">
+          <h3>Quem somos</h3>
+        </div>
+        <div className="descricao">
+          <h2>Fundação</h2>
+          <img src={images.livro} alt="" />
+          <p>
+            Lorem Ipsum é simplesmente uma simulação de texto da indústria
+            tipográfica e de impressos, e vem sendo utilizado desde o século
+            XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
+            os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum
+            sobreviveu não só a cinco séculos, como também ao salto para a
+            editoração eletrônica, permanecendo essencialmente inalterado. Se
+            popularizou na década de 60, quando a Letraset lançou decalques
+            contendo passagens de Lorem Ipsum, e mais recentemente quando passou
+            a ser integrado a softwares de editoração eletrônica como Aldus
+            PageMaker.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="rotulo">
+          <h3>Missão, visão e valores</h3>
+        </div>
+        <div className="descricao">
+          <Topico image={images.foguete} />
+          <Topico image={images.olho} />
+          <Topico image={images.coracao} />
+          <Marco img={images.instituicao} />
+          <Marco img={images.selo} />
+          <Marco img={images.titulo} />
+        </div>
+      </section>
+    </div>
+  );
 }
