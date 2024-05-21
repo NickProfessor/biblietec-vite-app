8059390c092007e3da465e000384724e494b7dce
Nickollas <nickollassilva90@gmail.com>
2024-05-21 14:26:18 -0300

    feat: add Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import "./_Footer.scss";
+
+export default function Footer() {
+  return (
+    <h3 className="texto-footer">
+      <i class="fa-regular fa-copyright"></i> BIBLIETEC - 2024
+    </h3>
+  );
+}
