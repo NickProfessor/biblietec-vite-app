0022a985bdacaa74e9d3923b8c4a2931fb521804
Nickollas <nickollassilva90@gmail.com>
2024-05-22 15:48:53 -0300

    feat: add a Livros page

--- src/pages/Livros/index.jsx
+++ src/pages/Livros/index.jsx
@@ -1,3 +1,30 @@
+import Livro from "../../components/Livro";
+import "./_Livros.scss";
 export default function Livros() {
-  return <h1>Livros</h1>;
+  return (
+    <section className="pagina-livros">
+      <div className="containerLivro">
+        <div className="texto-pesquise">
+          <h2>Conheça nossos livros!</h2>
+          <div className="input-pesquisar">
+            <span>
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </span>
+            <input type="text" name="" id="" placeholder="Pesquisar" />
+          </div>
+        </div>
+        <section className="secao-livros">
+          <Livro />
+          <Livro />
+          <Livro />
+          <Livro />
+          <Livro />
+          <Livro />
+          <Livro />
+          <Livro />
+        </section>
+        <button id="mostrarMais">Mostrar mais</button>
+      </div>
+    </section>
+  );
 }
