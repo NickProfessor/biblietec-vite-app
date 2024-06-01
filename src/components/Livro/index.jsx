02ae02874552092f3a0323d65d2be56abd2bfa38
Nickollas <nickollassilva90@gmail.com>
2024-06-01 02:01:13 -0300

    feat: add and testing data dinamic in component Livro

--- src/components/Livro/index.jsx
+++ src/components/Livro/index.jsx
@@ -1,12 +1,21 @@
 import images from "../../assets/images";
 import "./_Livro.scss";
 
-export default function Livro() {
+export default function Livro({ title, image, edicoes }) {
   return (
     <div className="livro">
-      <h3 id="nomeDoLivro">Harry Potter e a Pedra Filosofal</h3>
-      <img src={images.capa_harry} id="imagemDoLivro" />
-      <h3 id="preco">R$ 59,99</h3>
+      <h3 id="nomeDoLivro">{title}</h3>
+      <img
+        src={
+          image
+            ? `https://covers.openlibrary.org/b/id/${image}-M.jpg`
+            : images.imagem_padrao
+        }
+        id="imagemDoLivro"
+      />
+      <h3 id="preco">
+        {edicoes > 1 ? `${edicoes} Edições` : `${edicoes} Edição`}
+      </h3>
     </div>
   );
 }
--- src/pages/Livros/index.jsx
+++ src/pages/Livros/index.jsx
@@ -1,6 +1,27 @@
+import { useEffect } from "react";
 import Livro from "../../components/Livro";
 import "./_Livros.scss";
+import { useState } from "react";
 export default function Livros() {
+  const [livros, setLivros] = useState([0]);
+
+  const buscaLivros = async () => {
+    try {
+      const res = await fetch(
+        "https://openlibrary.org/search.json?q=machado+de+assis"
+      );
+      const data = await res.json();
+      const listaLivros = data.docs;
+      setLivros(listaLivros);
+      console.log(listaLivros);
+    } catch {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    buscaLivros();
+  }, []);
+
   return (
     <section className="pagina-livros">
       <div className="containerLivro">
@@ -14,14 +35,13 @@ export default function Livros() {
           </div>
         </div>
         <section className="secao-livros">
-          <Livro />
-          <Livro />
-          <Livro />
-          <Livro />
-          <Livro />
-          <Livro />
-          <Livro />
-          <Livro />
+          {livros.map((livro) => (
+            <Livro
+              title={livro.title}
+              image={livro.cover_i}
+              edicoes={livro.edition_count}
+            />
+          ))}
         </section>
         <button id="mostrarMais">Mostrar mais</button>
       </div>
