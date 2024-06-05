ffe205bd5a2f9874149f435b389fc47f6c8da3f4
Nickollas <nickollassilva90@gmail.com>
2024-06-05 18:10:29 -0300

    feat: add Modal component

--- src/components/Livro/index.jsx
+++ src/components/Livro/index.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import images from "../../assets/images";
 import "./_Livro.scss";
+import Modal from "../Modal";
 
 export default function Livro({ id, title, image, editions, author, subject }) {
   const [description, setDescription] = useState();
+  const [isModalVisible, setIsModalVisible] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -20,8 +23,13 @@ export default function Livro({ id, title, image, editions, author, subject }) {
 
     fetchData();
   }, [id]);
-  const exibeModal = async () => {
+  const exibeModal = () => {
+    setIsModalVisible(true);
     console.log(id, title, image, editions, author, subject, description);
+  };
+
+  const fechaModal = () => {
+    setIsModalVisible(false);
   };
   return (
     <div className="livro" onClick={exibeModal}>
@@ -37,6 +45,15 @@ export default function Livro({ id, title, image, editions, author, subject }) {
       <h3 id="preco">
         {editions > 1 ? `${editions} Edições` : `${editions} Edição`}
       </h3>
+      {isModalVisible &&
+        ReactDOM.createPortal(
+          <Modal onClose={fechaModal}>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <button onClick={fechaModal}>Close</button>
+          </Modal>,
+          document.getElementById("modal-root")
+        )}
     </div>
   );
 }
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,4 +1,42 @@
 import "./_Modal.scss";
+import images from "../../assets/images";
 export default function Modal() {
-  return <></>;
+  return (
+    <div className="pag-modal">
+      <div className="modal-content">
+        <i class="fa-solid fa-x"></i>
+        <aside className="book-visual">
+          <div className="book-image">
+            <img src={images.capa_harry} alt="" />
+          </div>
+          <div className="book-editions">10 edições</div>
+          <button className="book-link">Ver Livro</button>
+        </aside>
+        <section className="content-book">
+          <h2 className="book-title">Harry Potter E A Pedra Filosofal</h2>
+          <div className="keywords">
+            <h4 className="book-author">Por J.K. Rowling</h4>
+            <h4 className="book-keywords">Ficção Científica</h4>
+          </div>
+          <div className="description">
+            <h4 className="description-description">Descrição:</h4>
+            <p className="description-content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Recusandae sunt nostrum voluptatibus cumque doloremque maxime
+              consectetur, provident eligendi, ab inventore itaque quis. Hic
+              perspiciatis ut enim adipisci qui, ad consectetur.orem ipsum dolor
+              sit amet consectetur adipisicing elit. Recusandae sunt nostrum
+              voluptatibus cumque doloremque maxime consectetur, provident
+              eligendi, ab inventore itaque quis. Hic perspiciatis ut enim
+              adipisci qui, ad consectetur.orem ipsum dolor sit amet consectetur
+              adipisicing elit. Recusandae sunt nostrum voluptatibus cumque
+              doloremque maxime consectetur, provident eligendi, ab inventore
+              itaque quis. Hic perspiciatis ut enim adipisci qui, ad
+              consectetur.
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
