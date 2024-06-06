710704cb708ed225e31dd954eee90dee82cdfd64
Nickollas <nickollassilva90@gmail.com>
2024-06-06 18:21:27 -0300

    refactor: add props in Modal component

--- src/components/Livro/index.jsx
+++ src/components/Livro/index.jsx
@@ -7,14 +7,16 @@ import Modal from "../Modal";
 export default function Livro({ id, title, image, editions, author, subject }) {
   const [description, setDescription] = useState();
   const [isModalVisible, setIsModalVisible] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch("https://openlibrary.org" + id + ".json");
         const data = await res.json();
-        const description = data.description.value
-          ? data.description.value
-          : "descrição não está disponível";
+        const description =
+          typeof data.description === "string"
+            ? data.description
+            : data.description?.value || "descrição não está disponível";
         setDescription(description);
       } catch (error) {
         console.log(error);
@@ -23,6 +25,7 @@ export default function Livro({ id, title, image, editions, author, subject }) {
 
     fetchData();
   }, [id]);
+
   const exibeModal = () => {
     setIsModalVisible(true);
     console.log(id, title, image, editions, author, subject, description);
@@ -31,27 +34,35 @@ export default function Livro({ id, title, image, editions, author, subject }) {
   const fechaModal = () => {
     setIsModalVisible(false);
   };
+
   return (
-    <div className="livro" onClick={exibeModal}>
-      <h3 id="nomeDoLivro">{title}</h3>
-      <img
-        src={
-          image
-            ? `https://covers.openlibrary.org/b/id/${image}-M.jpg`
-            : images.imagem_padrao
-        }
-        id="imagemDoLivro"
-      />
-      <h3 id="preco">
-        {editions > 1 ? `${editions} Edições` : `${editions} Edição`}
-      </h3>
+    <div>
+      <div className="livro" onClick={exibeModal}>
+        <h3 id="nomeDoLivro">{title}</h3>
+        <img
+          src={
+            image
+              ? `https://covers.openlibrary.org/b/id/${image}-M.jpg`
+              : images.imagem_padrao
+          }
+          id="imagemDoLivro"
+        />
+        <h3 id="preco">
+          {editions > 1 ? `${editions} Edições` : `${editions} Edição`}
+        </h3>
+      </div>
       {isModalVisible &&
         ReactDOM.createPortal(
-          <Modal onClose={fechaModal}>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <button onClick={fechaModal}>Close</button>
-          </Modal>,
+          <Modal
+            onClose={fechaModal}
+            id={id}
+            title={title}
+            image={image}
+            editions={editions}
+            author={author}
+            subject={subject}
+            description={description}
+          />,
           document.getElementById("modal-root")
         )}
     </div>
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,39 +1,49 @@
 import "./_Modal.scss";
 import images from "../../assets/images";
-export default function Modal() {
+export default function Modal({
+  id,
+  title,
+  image,
+  editions,
+  author,
+  subject,
+  description,
+  onClose,
+}) {
+  const fechaModal = () => {
+    setIsModalVisible(false);
+  };
   return (
     <div className="pag-modal">
       <div className="modal-content">
-        <i class="fa-solid fa-x"></i>
+        <i class="fa-solid fa-x" onClick={onClose}></i>
         <aside className="book-visual">
           <div className="book-image">
-            <img src={images.capa_harry} alt="" />
+            <img
+              src={
+                image
+                  ? `https://covers.openlibrary.org/b/id/${image}-M.jpg`
+                  : images.imagem_padrao
+              }
+              alt=""
+            />
           </div>
-          <div className="book-editions">10 edições</div>
-          <button className="book-link">Ver Livro</button>
+          <div className="book-editions">
+            {editions > 1 ? `${editions} Edições` : `${editions} Edição`}
+          </div>
+          <button className="book-link" onClick={console.log("funcionou")}>
+            Ver Livro
+          </button>
         </aside>
         <section className="content-book">
-          <h2 className="book-title">Harry Potter E A Pedra Filosofal</h2>
+          <h2 className="book-title">{title}</h2>
           <div className="keywords">
-            <h4 className="book-author">Por J.K. Rowling</h4>
-            <h4 className="book-keywords">Ficção Científica</h4>
+            <h4 className="book-author">Por {author}</h4>
+            <h4 className="book-keywords">{subject}</h4>
           </div>
           <div className="description">
             <h4 className="description-description">Descrição:</h4>
-            <p className="description-content">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Recusandae sunt nostrum voluptatibus cumque doloremque maxime
-              consectetur, provident eligendi, ab inventore itaque quis. Hic
-              perspiciatis ut enim adipisci qui, ad consectetur.orem ipsum dolor
-              sit amet consectetur adipisicing elit. Recusandae sunt nostrum
-              voluptatibus cumque doloremque maxime consectetur, provident
-              eligendi, ab inventore itaque quis. Hic perspiciatis ut enim
-              adipisci qui, ad consectetur.orem ipsum dolor sit amet consectetur
-              adipisicing elit. Recusandae sunt nostrum voluptatibus cumque
-              doloremque maxime consectetur, provident eligendi, ab inventore
-              itaque quis. Hic perspiciatis ut enim adipisci qui, ad
-              consectetur.
-            </p>
+            <p className="description-content">{description}</p>
           </div>
         </section>
       </div>
