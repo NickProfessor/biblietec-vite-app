58ab1da24579297a217a324b8dec33179d69d89a
Nickollas <nickollassilva90@gmail.com>
2024-06-07 18:12:22 -0300

    feat: add logic in button "Motrar mais"

--- src/pages/Livros/index.jsx
+++ src/pages/Livros/index.jsx
@@ -2,29 +2,39 @@ import { useEffect, useRef } from "react";
 import Livro from "../../components/Livro";
 import "./_Livros.scss";
 import { useState } from "react";
+
 export default function Livros() {
   const [livros, setLivros] = useState([]);
+  const [limiteDeLivros, setLimiteDeLivros] = useState(20);
+  const [haLivrosNaPagina, setHaLivrosNaPagina] = useState(false);
+
+  const mostraMaisLivros = () => {
+    if (livros.length < 100) {
+      setLimiteDeLivros((prevLimite) => prevLimite + 20);
+    }
+  };
 
   const buscaLivros = async () => {
     try {
       const res = await fetch(
-        "https://openlibrary.org/search.json?q=harry+potter&fields=key,title,author_name,edition_count,cover_i,subject&limit=20"
+        `https://openlibrary.org/search.json?q=harry+potter&fields=key,title,author_name,edition_count,cover_i,subject&limit=${limiteDeLivros}`
       );
       const data = await res.json();
       const listaLivros = data.docs;
       setLivros(listaLivros);
+      setHaLivrosNaPagina(listaLivros.length > 0);
       console.log(listaLivros);
-      setHaLivrosNaPagina(true);
-    } catch {
+    } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     buscaLivros();
-  }, []);
+  }, [limiteDeLivros]);
 
-  const [haLivrosNaPagina, setHaLivrosNaPagina] = useState(false);
   const mostrarMaisRef = useRef(null);
+
   return (
     <section className="pagina-livros">
       <div className="containerLivro">
@@ -40,6 +50,7 @@ export default function Livros() {
         <section className="secao-livros">
           {livros.map((livro) => (
             <Livro
+              key={livro.key}
               id={livro.key}
               title={livro.title}
               image={livro.cover_i}
@@ -53,13 +64,16 @@ export default function Livros() {
             />
           ))}
         </section>
-        <button
-          id="mostrarMais"
-          className={haLivrosNaPagina ? "visivel" : ""}
-          ref={mostrarMaisRef}
-        >
-          Mostrar mais
-        </button>
+        {livros.length < 100 && (
+          <button
+            id="mostrarMais"
+            className={haLivrosNaPagina ? "visivel" : ""}
+            ref={mostrarMaisRef}
+            onClick={mostraMaisLivros}
+          >
+            Mostrar mais
+          </button>
+        )}
       </div>
     </section>
   );
